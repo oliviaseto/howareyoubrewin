040e39d0f6ff5a62348bd8ef3a2b31d30c56f079
Olivia Seto <oliviaseto5@gmail.com>
2023-10-21 18:31:38 -0400

    edited manifest to accomodate creation of extension

--- background.js
+++
@@ -1,7 +0,0 @@
-function handleClick(tab) {
-    chrome.tabs.executeScript(null, { file: "jquery-1.8.2.min.js" }, function() {
-        chrome.tabs.executeScript(null, { file: "content.js" });
-    });
-}
-
-chrome.pageAction.onClicked.addListener(handleClick);
